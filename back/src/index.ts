8785bc2f0f3b6932447c84184ab350f2b0ca1d07
ALEX-PC\Alex <alex_nguyen@live.ca>
2024-11-20 07:46:14 -0500

    compression of json

--- back/src/index.ts
+++ back/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import config from "../config.json";
 import { generateGame, toJson, turn } from "./game";
+import compression from "compression";
 const app = express();
 const port = config.port;
 generateGame(200);
@@ -9,6 +10,17 @@ turn();
 
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+app.use(compression({ filter: shouldCompress }));
+
+function shouldCompress(req, res) {
+  if (req.headers["x-no-compression"]) {
+    // don't compress responses with this request header
+    return false;
+  }
+
+  // fallback to standard filter function
+  return compression.filter(req, res);
+}
 app.use("/get", async (req, res, next) => {
   res.send(await toJson());
 });
